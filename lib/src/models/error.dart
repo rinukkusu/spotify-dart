457865f4b1f60940917ea13dab643521f0d64f2a
Chance Snow <git@chancesnow.me>
2018-02-10 08:16:28 -0800

    Update copyright year for new files

--- lib/src/models/error.dart
+++ lib/src/models/error.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, chances. All rights reserved. Use of this source code
+// Copyright (c) 2018, chances. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 part of spotify;
