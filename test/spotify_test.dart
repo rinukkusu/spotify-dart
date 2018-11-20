9b5863ba900bd8eae67d786aff91510ad3fcaf58
Hayri Bakici <hbi@medicuja.com>
2018-11-20 21:40:14 +0100

    - add default param value for searchType - renames method

--- test/spotify_test.dart
+++ test/spotify_test.dart
@@ -40,7 +40,7 @@ Future main() async {
 
   group('Search', () {
     test('get', () async {
-      var searchResult = await spotify.search.get('metallica', [SearchType.playlist, SearchType.artist]).first();
+      var searchResult = await spotify.search.get('metallica').first();
 
       expect(searchResult.length, 2);
     });
