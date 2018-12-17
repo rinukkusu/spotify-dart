29d0a7bd56503f74fc8b24ab3ae9251b3d3e36c1
Wesley Batista <wesley@civey.com>
2018-12-17 01:54:07 +0100

    Add docs

--- lib/src/models/player.dart
+++ lib/src/models/player.dart
@@ -8,16 +8,23 @@ class Player extends Object {
   Player() {}
   factory Player.fromJson(Map<String, dynamic> json) =>
       _$PlayerFromJson(json);
+  /// Unix Millisecond Timestamp when data was fetched
   int timestamp;
 
+  /// A [PlayerContext] Object. Can be [null]. 
   PlayerContext context;
 
+  /// Progress into the currently playing track. Can be [null].
   int progress_ms;
 
+  /// The currently playing track. Can be [null].
   Track item;
 
+  /// The object type of the currently playing item. Can be one of track,
+  /// episode, ad or unknown.
   String currently_playing_type;
   
+  /// If something is currently playing.
   bool is_playing;
 }
 
@@ -26,9 +33,17 @@ class PlayerContext extends Object {
   PlayerContext() {}
   factory PlayerContext.fromJson(Map<String, dynamic> json) =>
       _$PlayerContextFromJson(json);
+  
+  /// The external_urls of the context, or [null] if not available.
   ExternalUrls external_urls;
+
+  /// The href of the context, or [null] if not available.
   String href;
+
+  /// The object type of the item’s context. Can be one of album, artist or playlist.
   String type;
+
+  /// The uri of the context.
   String uri;  
 }
 
@@ -38,6 +53,7 @@ class ExternalUrls extends Object {
   factory ExternalUrls.fromJson(Map<String, dynamic> json) =>
       _$ExternalUrlsFromJson(json);
 
+  /// The Spotify URL for the object.
   String spotify;
   
 }
