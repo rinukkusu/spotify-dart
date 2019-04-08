fd7c0d01ec8bd2fab14ba86a319de5a2552f443c
rinukkusu <rinukkusu@sub-r.de>
2019-04-08 13:14:34 +0200

    add missing file

---
+++ lib/src/models/external_objects.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2019, rinukkusu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+part of spotify.models;
+
+@JsonSerializable(createToJson: false)
+class ExternalUrls extends Object {
+  ExternalUrls() {}
+  factory ExternalUrls.fromJson(Map<String, dynamic> json) =>
+      _$ExternalUrlsFromJson(json);
+
+  /// The Spotify URL for the object.
+  String spotify;
+}
+
+@JsonSerializable(createToJson: false)
+class ExternalIds extends Object {
+  ExternalIds() {}
+  factory ExternalIds.fromJson(Map<String, dynamic> json) =>
+      _$ExternalIdsFromJson(json);
+
+  /// International Standard Recording Code
+  String isrc;
+
+  /// International Article Number
+  String ean;
+
+  /// Universal Product Code
+  String upc;
+}
