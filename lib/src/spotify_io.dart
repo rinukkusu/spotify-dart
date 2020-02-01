0503e19400333d92f707afa6d17cc1212ada48b3
Roi Snir <roisnr132@gmail.com>
2020-02-01 18:23:10 +0200

    bugfix + example refactored

--- lib/src/spotify_io.dart
+++ lib/src/spotify_io.dart
@@ -11,7 +11,7 @@ class SpotifyApi extends SpotifyApiBase {
   static Future<SpotifyApi> fromCredentials(SpotifyApiCredentials credentials) async {
 
     final client = await clientCredentialsGrant(
-      Uri.parse(SpotifyApiBase._authorizationUrl),
+      Uri.parse(SpotifyApiBase._tokenUrl),
       credentials.clientId, credentials.clientSecret);
     return SpotifyApi(client);
   }
