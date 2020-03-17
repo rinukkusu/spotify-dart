e7ca9cb0603fa81ae8cca91c006d1a9a22b19773
Roi Snir <roisnr132@gmail.com>
2020-03-17 17:51:09 +0200

    added batches

---
+++ lib/src/utils.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2017, rinukkusu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+part of spotify;
+
+Iterable<List<T>> batches<T>(Iterable<T> source, int size) sync* {
+  List<T> accumulator;
+  for (var value in source) {
+    (accumulator ??= []).add(value);
+    if (accumulator.length == size) {
+      yield accumulator;
+      accumulator = null;
+    }
+  }
+  if (accumulator != null) yield accumulator;
+}
