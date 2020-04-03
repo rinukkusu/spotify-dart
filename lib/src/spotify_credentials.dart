c14a314fb238adf594b8c88828ca1eceb94e77ae
Levi Hassel <levihassel@gmail.com>
2020-04-03 17:35:28 -0500

    Updated summary for SpotifyApiCredentials expiration

--- lib/src/spotify_credentials.dart
+++ lib/src/spotify_credentials.dart
@@ -35,7 +35,8 @@ class SpotifyApiCredentials {
   /// for a full list of available scopes.
   List<String> scopes;
 
-  /// The date at which these credentials will expire.
+  /// The date at which these credentials will expire, stored in the user's
+  /// local time.
   ///
   /// This is likely to be a few seconds earlier than the server's idea of the
   /// expiration date.
