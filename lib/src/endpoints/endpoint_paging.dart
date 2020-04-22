8b6e8105d62648f4eb5df61a4e3061ff1ba638e9
Roi Snir <roisnr132@gmail.com>
2020-04-22 03:16:53 +0300

    fixed fromPaging bug

--- lib/src/endpoints/endpoint_paging.dart
+++ lib/src/endpoints/endpoint_paging.dart
@@ -76,7 +76,7 @@ class Pages<T> extends _Pages<Page<T>> {
 
   Pages.fromPaging(SpotifyApi api, Paging<T> paging, this._pageParser,
                  [String pageKey,
-                   ParserFunction<Object> pageContainerMapper]) : super(api, Uri.parse(paging.href).path, pageKey, pageContainerMapper){
+                   ParserFunction<Object> pageContainerMapper]) : super(api, Uri.parse(paging.href).path.substring(1), pageKey, pageContainerMapper){
     _bufferedPages.add(Page<T>(paging, _pageParser));
   }
 
