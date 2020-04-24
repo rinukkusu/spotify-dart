519a37729257b750272429e1ab1bbc533c3565b9
Maximilian Riegler <maximilian.riegler@myworld.com>
2020-04-24 19:20:44 +0200

    Move 'me' related calls to own endpoint class

--- lib/src/spotify_base.dart
+++ lib/src/spotify_base.dart
@@ -10,41 +10,47 @@ abstract class SpotifyApiBase {
       'https://accounts.spotify.com/authorize';
 
   FutureOr<oauth2.Client> _client;
-  Artists _artists;
-  Albums _albums;
-  Tracks _tracks;
-  Playlists _playlists;
-  RecommendationsEndpoint _recommendations;
-  Users _users;
-  Search _search;
-  AudioFeatures _audioFeatures;
-  Categories _categories;
 
+  Artists _artists;
   Artists get artists => _artists;
 
+  Albums _albums;
   Albums get albums => _albums;
 
+  Tracks _tracks;
   Tracks get tracks => _tracks;
 
+  Playlists _playlists;
   Playlists get playlists => _playlists;
 
+  RecommendationsEndpoint _recommendations;
   RecommendationsEndpoint get recommendations => _recommendations;
 
+  Users _users;
   Users get users => _users;
 
+  Search _search;
   Search get search => _search;
 
+  AudioFeatures _audioFeatures;
   AudioFeatures get audioFeatures => _audioFeatures;
+
+  Categories _categories;
   Categories get categories => _categories;
+
+  Me _me;
+  Me get me => _me; 
 
   SpotifyApiBase.fromClient(FutureOr<http.BaseClient> client) {
     _client = client;
+
     _artists = Artists(this);
     _albums = Albums(this);
     _tracks = Tracks(this);
     _playlists = Playlists(this);
     _recommendations = RecommendationsEndpoint(this);
-    _users = Users(this);
+    _me = Me(this);
+    _users = Users(this, _me);
     _search = Search(this);
     _audioFeatures = AudioFeatures(this);
     _categories = Categories(this);
