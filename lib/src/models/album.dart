6a907f92969e88eb2fac77f0c2f2416d657c35c9
Hayri Bakici <hayri.bakici@gmail.com>
2020-11-16 18:24:39 +0100

    checks for empty tracks

--- lib/src/models/album.dart
+++ lib/src/models/album.dart
@@ -98,10 +98,11 @@ class AlbumSimple extends Object {
   /// The Spotify URI for the album.
   String uri;
 
-  /// The tracks of this album
+  /// The tracks of this album wrapped in a page object.
   @JsonKey(name: 'tracks')
   Paging<TrackSimple> tracksPage;
 
   Iterable<TrackSimple> get tracksNative =>
-      tracksPage.itemsNative.map((e) => TrackSimple.fromJson(e)).toList();
+      tracksPage?.itemsNative?.map((e) => TrackSimple.fromJson(e))?.toList() ??
+      [];
 }
