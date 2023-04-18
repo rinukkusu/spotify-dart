004f3dbd55445b9f972d9a8751f6028e2e895c22
Hayri Bakici <hayri.bakici@gmail.com>
2023-04-18 19:19:03 +0200

    fixes another test

--- test/spotify_test.dart
+++ test/spotify_test.dart
@@ -488,9 +488,9 @@ Future main() async {
       var result = await spotify.tracks.me.containsTracks(['1', '2', '3']);
 
       expect(result.length, 3);
-      expect(result[0], isTrue);
-      expect(result[1], isFalse);
-      expect(result[2], isTrue);
+      expect(result['1'], isTrue);
+      expect(result['2'], isFalse);
+      expect(result['3'], isTrue);
     });
   });
 
