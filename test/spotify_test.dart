7eb8cf5b1495fd612ae8aaa9b30d86eec741d6ae
Hayri Bakici <hayri.bakici@gmail.com>
2023-05-08 10:56:05 +0200

    adds album tracks test

--- test/spotify_test.dart
+++ test/spotify_test.dart
@@ -21,29 +21,28 @@ Future main() async {
     });
 
     test('album tracks', () async {
-      var album = await spotify.albums.get('4aawyAB9vmqN3uQ7FjRGTy');
-      var items = album.tracks;
-      expect(items!.length, 3);
+      var tracksPage = spotify.albums.tracks('4aawyAB9vmqN3uQ7FjRGTy');
+      var firstPage = await tracksPage.first();
 
-      var tracks = album.tracks;
-
-      var trackOne = tracks!.first;
+      var trackOne = firstPage.items!.first;
       expect(trackOne.discNumber, 1);
       expect(trackOne.durationMs, 85400);
       expect(trackOne.id, '6OmhkSOpvYBokMKQxpIGx2');
-      expect(trackOne.isPlayable, true);
+      expect(trackOne.isPlayable, isNull);
       expect(trackOne.type, 'track');
       expect(trackOne.uri, 'spotify:track:6OmhkSOpvYBokMKQxpIGx2');
       expect(trackOne.explicit, true);
       expect(trackOne.href,
           'https://api.spotify.com/v1/tracks/6OmhkSOpvYBokMKQxpIGx2');
       expect(trackOne.previewUrl,
-          'https://p.scdn.co/mp3-preview/bf9e33b1bb53c281c5eea0da6c317f2cd7c3eb58?cid=8897482848704f2a8f8d7c79726a70d4');
-      expect(trackOne.name, 'Global Warming');
+          'https://p.scdn.co/mp3-preview/81b57845f672fa5a0af749489e311ffb9fd552fe?cid=0b297fa8a249464ba34f5861d4140e58');
+      expect(trackOne.name, 'Global Warming (feat. Sensato)');
 
       expect(trackOne.externalUrls != null, true);
       expect(trackOne.externalUrls!.spotify,
           'https://open.spotify.com/track/6OmhkSOpvYBokMKQxpIGx2');
+      expect(trackOne.availableMarkets, isNotNull);
+      expect(trackOne.availableMarkets?.first, Market.AT);
 
       var artists = trackOne.artists;
       expect(artists!.length, 2);
