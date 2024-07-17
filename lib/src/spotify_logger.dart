398663efc29ce95985e1a224903c3a7910a93eab
Hayri Bakici <hayri.bakici@gmail.com>
2024-07-17 23:54:16 +0200

    adds licence header

--- lib/src/spotify_logger.dart
+++ lib/src/spotify_logger.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2024, hayribakici. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 part of '../spotify.dart';
 
 abstract class SpotifyLogger {
