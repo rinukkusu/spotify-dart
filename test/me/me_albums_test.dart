017778e1fb99a1c62b85248dd2a90680129bfb60
Hayri Bakici <hayri.bakici@gmail.com>
2025-12-31 10:37:29 +0100

    adds ignore file

--- test/me/me_albums_test.dart
+++ test/me/me_albums_test.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import '../spotify_mock.dart';
 import 'package:test/test.dart';
 
-import 'me_core_test.dart';
+import 'me_core_test.dart.ignored';
 
 Future main() async {
   final spotify = SpotifyApiMock.create();
