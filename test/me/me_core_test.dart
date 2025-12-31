9e5e0ad48fddff6b21da82a4dc49b768530f70a7
Hayri Bakici <hayri.bakici@gmail.com>
2025-12-31 09:35:09 +0100

    fixes tests

--- test/me/me_core_test.dart
+++ test/me/me_core_test.dart
@@ -1,32 +1,36 @@
+// ignore_for_file: unused_element
+
 import 'dart:async';
 
 import 'package:spotify/spotify.dart';
 
 import 'package:test/test.dart';
 
-Future<void> testSaveThrowsOnEmptyIds(LibraryModifiable m) async {
-  expect(() => m.save(List.empty()), throwsA(isA<ArgumentError>()));
-}
+Future main() async {
+  Future<void> testSaveThrowsOnEmptyIds(LibraryModifiable m) async {
+    expect(() => m.save(List.empty()), throwsA(isA<ArgumentError>()));
+  }
 
-Future<void> testSaveThrowsOnGreaterThanN(LibraryModifiable m, int n) async {
-  final tooMany = List.generate(n + 1, (i) => 'id$i');
-  expect(() => m.save(tooMany), throwsA(isA<RangeError>()));
-}
+  Future<void> testSaveThrowsOnGreaterThanN(LibraryModifiable m, int n) async {
+    final tooMany = List.generate(n + 1, (i) => 'id$i');
+    expect(() => m.save(tooMany), throwsA(isA<RangeError>()));
+  }
 
-Future<void> testContainsThrowsOnEmptyIds(LibraryModifiable m) async {
-  expect(() => m.contains(List.empty()), throwsA(isA<ArgumentError>()));
-}
+  Future<void> testContainsThrowsOnEmptyIds(LibraryModifiable m) async {
+    expect(() => m.contains(List.empty()), throwsA(isA<ArgumentError>()));
+  }
 
-Future<void> testContainsThrowsOnGreaterThanN(LibraryModifiable m, int n) async {
-  final tooMany = List.generate(n + 1, (i) => 'id$i');
-  expect(() => m.contains(tooMany), throwsA(isA<RangeError>()));
-}
+  Future<void> testContainsThrowsOnGreaterThanN(LibraryModifiable m, int n) async {
+    final tooMany = List.generate(n + 1, (i) => 'id$i');
+    expect(() => m.contains(tooMany), throwsA(isA<RangeError>()));
+  }
 
-Future<void> testRemoveThrowsOnEmptyIds(LibraryModifiable m) async {
-  expect(() => m.remove(List.empty()), throwsA(isA<ArgumentError>()));
-}
+  Future<void> testRemoveThrowsOnEmptyIds(LibraryModifiable m) async {
+    expect(() => m.remove(List.empty()), throwsA(isA<ArgumentError>()));
+  }
 
-Future<void> testRemoveThrowsOnGreaterThanN(LibraryModifiable m, int n) async {
-  final tooMany = List.generate(n + 1, (i) => 'id$i');
-  expect(() => m.remove(tooMany), throwsA(isA<RangeError>()));
+  Future<void> testRemoveThrowsOnGreaterThanN(LibraryModifiable m, int n) async {
+    final tooMany = List.generate(n + 1, (i) => 'id$i');
+    expect(() => m.remove(tooMany), throwsA(isA<RangeError>()));
+  }
 }
